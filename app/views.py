6cd9977e3d31ba035d35df5a6cff344ad5590478
Vishnuvaradhan123 <vishnuvaradhanp99@gmail.com>
2026-09-14 05:50:21 -0700

    Update Django home page

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Hi Dude TMA VISHNU AND HOGRIDER " \
+    "" \
+    "" \
+    "" \
+    "RKGO RGNBR" 
+    "R" \
+    "TRG" 
+    "TRTR" 
+    "GRT" 
+    "GTR"
+     "cfedfv"
+     "efrce")
