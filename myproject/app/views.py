f654fbed4191cdadf332eda76446e370ff4c851b
Vishnuvaradhan123 <vishnuvaradhanp99@gmail.com>
2026-09-14 05:42:11 -0700

    Update home page greeting

--- myproject/app/views.py
+++ myproject/app/views.py
@@ -5,4 +5,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hi Dude")
+    return HttpResponse("Hi Dude TMA VISHNU AND HOGRIDER " \
+    "" \
+    "" \
+    "" \
+    "RKGO RGNBR" \
+    "R" \
+    "TRG" \
+    "TRTR" \
+    "GRT" \
+    "GTR")
