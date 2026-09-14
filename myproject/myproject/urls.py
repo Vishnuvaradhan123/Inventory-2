a29c24c5304391cff85466a4c90be93ac0f37dde
Vishnuvaradhan123 <114336385+Vishnuvaradhan123@users.noreply.github.com>
2026-09-14 04:38:31 -0700

    Fix app import path for Render host

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,10 +1,7 @@
-"""
-URL configuration for myproject project.
-"""
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('', include('myproject.app.urls')),
 ]
